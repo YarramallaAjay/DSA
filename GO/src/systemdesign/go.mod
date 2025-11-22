84135804fd950563cdba2dbf1e6e6cc17a4fe008
Ajay Yarramalla <yarramallaajay@gmail.com>
2025-11-22 23:43:03 +0530

    made changes to repo and started practicing with go modules

---
+++ GO/src/systemdesign/go.mod
@@ -0,0 +1,3 @@
+module github.com/YarramallaAjay/main
+
+go 1.25.1
---
+++ GO/src/systemdesign/workerpool.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type WorkerPool struct {
+	PoolSize int
+	Task     *func()
+	wg       *sync.WaitGroup
+	mx       *sync.Mutex
+}
+
+type Job struct {
+	Id    int
+	Value int
+}
+
+type Result struct {
+	Id     int
+	Square int
+}
+
+func Worker(id int, jobs <-chan Job, results chan<- Result, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	for j := range jobs {
+		fmt.Printf("worker: %v is working with job : %v \n", id, j.Id)
+		val := j.Value
+		results <- Result{
+			Id:     j.Id,
+			Square: val * 2,
+		}
+
+	}
+}
+
+func CollectResults(Results <-chan Result, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for r := range Results {
+		fmt.Printf("Job %v completed and received the response as: %v \n", r.Id, r.Square)
+	}
+}
+
+func Dispatcher(jobsCount int, workersCount int) {
+
+	jobs := make(chan Job)
+	results := make(chan Result)
+
+	var wg sync.WaitGroup
+
+	wg.Add(workersCount)
+
+	for i := 0; i < workersCount; i++ {
+
+		go Worker(i, jobs, results, &wg)
+
+	}
+
+	var resultWg sync.WaitGroup
+
+	resultWg.Add(1)
+
+	go CollectResults(results, &resultWg)
+
+	for i := 0; i <= jobsCount; i++ {
+		jobs <- Job{
+			Id:    i,
+			Value: i + 1,
+		}
+	}
+
+	close(jobs)
+
+	wg.Wait()
+
+	close(results)
+
+	resultWg.Wait()
+
+}
+
+func main() {
+
+	var jobsCount int = 100
+	var workerCount int = 5
+
+	Dispatcher(jobsCount, workerCount)
+
+}
